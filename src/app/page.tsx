0f0254e10b5bfd0e13373ea68981c549c6e4f802
jariz <j.zwarts@atabix.nl>
2024-10-09 21:19:01 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function Component() {
   const futureSessions = bookings.splice(1);
   const zonedNow = toZonedTime(new Date(), "Europe/Amsterdam");
   const nextZonedDate = toZonedTime(nextSession?.date, "Europe/Amsterdam");
-  const shouldDoConfetti = isBefore(zonedNow, nextZonedDate);
+  const shouldDoConfetti = isBefore(nextZonedDate, zonedNow);
 
   return (
     <div>
